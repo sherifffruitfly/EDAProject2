667ebe0f85bc2c4504072c23185b6ee5c60b3df7
sherifffruitfly <sherifffruitfly@gmail.com>
2017-08-21 10:56:09 -0700

    decent plot. need to break out by year on multiple panels, and make the legend numbers mean something.
    maybe also somehow normalize emissions to some kind of % of total or something, rather than log.
    
    lol might hafto log that percentage anyway tho, in order to flatten extemes

--- Plot1.R
+++ Plot1.R
@@ -3,7 +3,13 @@
 library("maps")
 library("mapproj")
 data(county.fips)
-colors = c("#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043")
+
+numbreaks <- 5
+startcolor <- "white"
+midcolor <- "orange"
+endcolor <- "red"
+colfunc <- colorRampPalette(c(startcolor, midcolor, endcolor))
+colors = colfunc(numbreaks)
 
 source("DataLoader.R")
 pd <- loadPollutionData(verbose=TRUE, joinTables = FALSE)
@@ -11,23 +17,36 @@ pd <- loadPollutionData(verbose=TRUE, joinTables = FALSE)
 emissionsum <- aggregate(pd$pol$Emissions, by=list(pd$pol$year, pd$pol$fips), FUN=sum)
 # group1=year, group2=fips, x=emissions
 
-emissionsum$logsum <- log(emissionsum$x)
+emissionsum$logsum <- log(1 + emissionsum$x)
 emissionsum$fips <- as.numeric(emissionsum$Group.2)
-emissionsum$colorbuckets <- as.numeric(cut(log(1+ emissionsum$x), breaks=7))
-
+emissionsum$colorbuckets <- as.numeric(cut(emissionsum$logsum, breaks=numbreaks))
+#emissionsum$colorbuckets <- as.numeric(cut(emissionsum$x, breaks=numbreaks))
 colsmatched <- emissionsum$colorbuckets[match(county.fips$fips, emissionsum$fips)]
 
+#png("Plot1.png")
+plot.new()
 map("county"
     , col = colors[colsmatched]
     , fill = TRUE
     , resolution = 0
     , lty = 0
     , projection = "polyconic")
-map("world", c("hawaii"), boundary = TRUE, col=8, add = TRUE, fill=TRUE )
-map("world", c("USA:Alaska"), boundary = TRUE, col='orange', add = TRUE, fill=TRUE )
+map("state"
+    , col = "white"
+    , fill = FALSE
+    , add = TRUE
+    , lty = 1
+    , lwd = 0.2
+    , projection = "polyconic")
+leg.txt <- as.character(c(1:numbreaks))
+legend("bottom"
+       , legend=as.character(leg.txt)
+       #, horiz = TRUE
+       , fill = colors
+       , ncol=min(10, numbreaks)) #gives max 10 legend columns before wrapping to another row
+title("log(PM25 Emissions) Across The US By County")
 
-#png("Plot1.png")
+#map("world", c("hawaii"), boundary = TRUE, col=8, add = TRUE, fill=TRUE )
+#map("world", c("USA:Alaska"), boundary = TRUE, col='orange', add = TRUE, fill=TRUE )
 
-
-
-dev.off()
+#dev.off()
