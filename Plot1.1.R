f2821b18c56bd3be10e6595204b4c6c4e56d17cf
sherifffruitfly <sherifffruitfly@gmail.com>
2017-08-22 17:36:35 -0700

    good answer to q1 - need lm line tho.

--- Plot1.1.R
+++ Plot1.1.R
@@ -13,18 +13,17 @@ emissionsum$value <- log(1 + emissionsum$x) # log(1+emissions)
 #emissionsum$value <- emissionsum$x # emissions actual value
 emissionsum$region <- as.numeric(emissionsum$Group.2)
 
-# get unique region values for choropeth call
 # year values: 1999, 2002, 2005, 2008
-plotYear <- 2002
-message("fitlering for ", plotYear)
-emissionsum <- emissionsum[emissionsum$Group.1 == plotYear,]
+plotYear <- 1999
 
 # get rid of rows that don't map to fips values in choropeth
 message("tossing unmappable regions")
 emissionsum <- merge(emissionsum, county.regions, by="region")
 
 message("constructing plot")
-choro = CountyChoropleth$new(emissionsum)
-choro$title = paste("                                 PM25 Emissions Across The US By County, ", plotYear)
+choro = CountyChoropleth$new(emissionsum[emissionsum$Group.1 == plotYear,])
+choro$title = bquote("                                 " ~ PM[2.5] ~ "Emissions Across The U.S. By County," ~ .(plotYear))
 choro$ggplot_scale = scale_fill_brewer(name="Emissions", palette=7, drop=FALSE)
+choro$warn <- FALSE
+choro$set_num_colors(9)
 choro$render()
--- Plot1.R
+++ Plot1.R
@@ -1,52 +1,24 @@
-# Histogram of GAP for 2/1/2007-2/2/2007
-
-library("maps")
-library("mapproj")
-
-numbreaks <- 5
-startcolor <- "white"
-midcolor <- "orange"
-endcolor <- "red"
-colfunc <- colorRampPalette(c(startcolor, midcolor, endcolor))
-colors = colfunc(numbreaks)
-
 source("DataLoader.R")
-pd <- loadPollutionData(verbose=TRUE, joinTables = FALSE)
+message("loading data")
+if(nrow(pd) != 6497651){
+  pd <- loadPollutionData(verbose=FALSE, joinTables = TRUE)
+}
 
-emissionsum <- aggregate(pd$pol$Emissions, by=list(pd$pol$year, pd$pol$fips), FUN=sum)
-# group1=year, group2=fips, x=emissions
+message("aggregating by year")
+emissionsum <- aggregate(pd$Emissions, by=list(pd$year, pd$type), FUN=sum)
+names(emissionsum) <- c("Year", "Type", "Emissions")
+matrixemissions <- as.matrix(as.data.frame.matrix(xtabs(Emissions/1000000 ~ Type + Year, emissionsum)))
 
-emissionsum$logsum <- log(1 + emissionsum$x)
-emissionsum$fips <- as.numeric(emissionsum$Group.2)
-emissionsum$colorbuckets <- as.numeric(cut(emissionsum$logsum, breaks=numbreaks))
-#emissionsum$colorbuckets <- as.numeric(cut(emissionsum$x, breaks=numbreaks))
-colsmatched <- emissionsum$colorbuckets[match(county.fips$fips, emissionsum$fips)]
-
-#png("Plot1.png")
 plot.new()
-map("world", c("hawaii"), xlim=c(-180,-65), ylim=c(19,72),interior = FALSE)
-map("county"
-    , col = colors[colsmatched]
-    , fill = TRUE
-    , resolution = 0
-    , lty = 0
-    , projection = "polyconic")
-map("state"
-    , col = "white"
-    , fill = FALSE
-    , add = TRUE
-    , lty = 1
-    , lwd = 0.2
-    , projection = "polyconic")
-leg.txt <- as.character(c(1:numbreaks))
-legend("bottom"
-       , legend=as.character(leg.txt)
-       #, horiz = TRUE
-       , fill = colors
-       , ncol=min(10, numbreaks)) #gives max 10 legend columns before wrapping to another row
-title("log(PM25 Emissions) Across The US By County")
-
-#map("world", c("hawaii"), boundary = TRUE, col=8, add = TRUE, fill=TRUE )
-#map("world", c("USA:Alaska"), boundary = TRUE, col='orange', add = TRUE, fill=TRUE )
-
-#dev.off()
+plotcolors <- c("#f7fcb9", "#7fcdbb", "#bcbddc", "sandybrown")
+barplot(matrixemissions
+        , main=bquote(PM[2.5] ~ "Emissions For The U.S. By Year")
+        , col=plotcolors
+        , xlab="Year"
+        , ylab="Emissions (millions of tons)"
+        )
+legend("topright"
+       , legend=rownames(matrixemissions)
+       , title="Type"
+       , fill=plotcolors
+       )
