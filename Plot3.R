f058955364c3f4282dd55d99d7799f634c8fd1c0
sherifffruitfly <sherifffruitfly@gmail.com>
2017-08-23 22:55:19 -0700

    getting close. make groups tighter, and put % change on top of the bars and i'm done.

--- Plot3.R
+++ Plot3.R
@@ -1,27 +1,39 @@
-# Sub metering plots over 2/1/2007-2/2/2007
+# ggplot2 for baltimore city md emissions changes by type year to year
 
+library(grid)
+library("ggplot2")
 source("DataLoader.R")
-plotdata <- loadData()
 
-png("Plot3.png")
+if(nrow(pd) != 13624){
+  message("loading data")
+  pd <- loadPollutionData(verbose=FALSE, joinTables = TRUE)
+  pd <- pd[pd$fips_N == 24510,]
+}
 
-plot(plotdata$datetime, plotdata$Sub_metering_1
-     , type = "l"
-     , xlab = ""
-     , ylab = "Energy sub metering"
-     )
-points(plotdata$datetime, plotdata$Sub_metering_2
-       , type = "l"
-       , col = "red"
-       )
-points(plotdata$datetime, plotdata$Sub_metering_3
-       , type = "l"
-       , col = "blue"
-       )
-legend("topright"
-       , lty = 1
-       , col = c("black", "red", "blue")
-       , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-       )
+message("aggregating by year")
+emissionsum <- aggregate(pd$Emissions, by=list(pd$year, pd$type), FUN=sum)
+names(emissionsum) <- c("Year", "Type", "Emissions")
 
-dev.off()
+
+myplot <- ggplot(emissionsum
+             , aes(interaction(Year, Type), Emissions, fill=Type)) + 
+        geom_bar(width = 0.5
+                 , position = position_dodge(width=.1)
+                 , stat="identity"
+        ) + 
+        coord_cartesian(ylim = c(0, 2500), expand = FALSE) +
+        annotate(geom = "text", x = seq_len(nrow(emissionsum)), y = -40, label = emissionsum$Year, size = 3) +
+        annotate(geom = "text", x = 2.5 + 4 * (0:3), y = -130, label = unique(emissionsum$Type), size = 4) +
+        theme_bw() +
+        theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
+              axis.title.x = element_blank(),
+              axis.text.x = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        theme(legend.position = "none")
+
+
+# make a grob, turn off clipping
+myplot2 <- ggplot_gtable(ggplot_build(myplot))
+myplot2$layout$clip[myplot2$layout$name == "panel"] <- "off"
+grid::grid.draw(myplot2)
