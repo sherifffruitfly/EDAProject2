b1917be87e1dd5d00170d38e0ffee711e92b8d06
sherifffruitfly <sherifffruitfly@gmail.com>
2017-08-24 00:59:00 -0700

    good - need to add absolute emissions value for the first bar on each type, and call it a day

--- Plot3.R
+++ Plot3.R
@@ -1,5 +1,7 @@
 # ggplot2 for baltimore city md emissions changes by type year to year
 
+library("scales")
+library(data.table)
 library(grid)
 library("ggplot2")
 source("DataLoader.R")
@@ -10,27 +12,41 @@ if(nrow(pd) != 13624){
   pd <- pd[pd$fips_N == 24510,]
 }
 
-message("aggregating by year")
+message("aggregating by year/type")
 emissionsum <- aggregate(pd$Emissions, by=list(pd$year, pd$type), FUN=sum)
 names(emissionsum) <- c("Year", "Type", "Emissions")
 
+# add in year over year column
+temp <- split(emissionsum, emissionsum$Type)
+temp <- lapply(temp, function(y) transform(y, new.col=c(NA,Emissions[-1]/Emissions[-nrow(y)]-1)))
+emissionsum <- rbindlist(temp)
+names(emissionsum)[4] <- "YoY"
 
+# plot
 myplot <- ggplot(emissionsum
-             , aes(interaction(Year, Type), Emissions, fill=Type)) + 
-        geom_bar(width = 0.5
-                 , position = position_dodge(width=.1)
-                 , stat="identity"
-        ) + 
-        coord_cartesian(ylim = c(0, 2500), expand = FALSE) +
-        annotate(geom = "text", x = seq_len(nrow(emissionsum)), y = -40, label = emissionsum$Year, size = 3) +
-        annotate(geom = "text", x = 2.5 + 4 * (0:3), y = -130, label = unique(emissionsum$Type), size = 4) +
-        theme_bw() +
-        theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
-              axis.title.x = element_blank(),
-              axis.text.x = element_blank(),
-              panel.grid.major.x = element_blank(),
-              panel.grid.minor.x = element_blank()) +
-        theme(legend.position = "none")
+             , aes(interaction(Year, Type), Emissions)) + 
+          geom_bar(aes(fill=Type)
+                   , width = 0.5
+                   #, position = "dodge"
+                   , stat="identity"
+          ) + 
+          coord_cartesian(ylim = c(0, 2500), expand = FALSE) +
+          annotate(geom = "text", x = seq_len(nrow(emissionsum)), y = -50, label = emissionsum$Year, size = 3) +
+          annotate(geom = "text", x = 2.5 + 4 * (0:3), y = -150, label = unique(emissionsum$Type), size = 3.5) +
+          theme(panel.background = element_rect(fill="gray", color = "gray")) +
+          theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
+                axis.title.x = element_blank(),
+                axis.text.x = element_blank(),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank()) +
+          theme(legend.position = "none") +
+          geom_text(aes(label = ifelse(emissionsum$YoY >= 0, paste("+", percent(emissionsum$YoY), sep=""), percent(emissionsum$YoY)), vjust=-1)
+                    , size=2.5
+                    , col = ifelse(emissionsum$YoY > 0, "darkred", "darkgreen")
+                    ) + 
+          labs(y = "Emissions (tons)") +
+          theme(plot.title = element_text(hjust = 0.5)) + 
+          ggtitle("Emissions Changes For Baltimore City, MD By Type, 1999-2008")
 
 
 # make a grob, turn off clipping
